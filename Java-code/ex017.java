c61111c6d17f8587f6c527f10145e75df7afcb29
boolks <bootre@naver.com>
2019-01-12 11:05:14 +0900

    Create ex017.java

---
+++ Java-code/ex017.java
@@ -0,0 +1,24 @@
+package ko.co.infopub.chapter.s017;
+
+class JLocation{
+	public double lat;
+	public double lng;
+}
+
+public class JavaUserDefinedType {
+
+	public static void main(String[] args) {
+		// 두 개의 기본 타입
+		double latitude = 37.52127220511242;
+		double longitude = 127.0074462890625;
+		// 사용자 정의 객체를 생성
+		JLocation jloc = new JLocation();
+		jloc.lat = latitude;
+		jloc.lng = longitude;
+		
+		JLocation newLoc = jloc;	// 객체를 대입
+		System.out.println(newLoc.lat);
+
+	}
+
+}
