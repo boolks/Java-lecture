e6469e588aeca980c9105a77ef2c38fa25a5c518
boolks <bootre@naver.com>
2019-01-12 11:56:31 +0900

    메서드 선언하고 호출하기

---
+++ Java-code/ex024.java
@@ -0,0 +1,15 @@
+package ko.co.infopub.chapter.s024;
+
+public class Hello {
+	
+	public static void showHelp() {
+		System.out.println("************************************");
+		System.out.println("이 프로그래밍은 Java200이 만들었습니다.");
+		System.out.println("************************************");
+	}
+	
+	public static void main(String[] args) {
+		showHelp();
+	}
+
+}
